dbc126b51f008beb2415af187f9b442b5545f94b
marlyne <=>
2024-05-24 14:26:17 +0200

    create a binary tree node

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -1,7 +1,12 @@
+#ifndef MAX
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#endif
+
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -20,5 +25,11 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s heap_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t * find_parent_node(heap_t *root, int height);
+void swap_nodes(heap_t *node1, heap_t *node2);
+int height_tree(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
