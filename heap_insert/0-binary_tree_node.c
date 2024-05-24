78e09e2e93924d71504825527a088beeb9bdc92a
marlyne <=>
2024-05-24 14:51:37 +0200

    create a binary tree node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -3,10 +3,10 @@
 
 /**
  * binary_tree_node - Creates a binary tree node
- * @parent: pointer to the parent node of the node to create
- * @value: value to put in the new node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to put in the new node
  *
- * return: pointer to the new node, or NULL on failure
+ * Return: pointer to the new node, or NULL on failure
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
