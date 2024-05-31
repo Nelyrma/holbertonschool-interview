594dfb90a05b99d7dac7a03b688bdba8c0a87f3b
marlyne <=>
2024-05-31 11:36:27 +0200

    comment the module

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -13,14 +13,14 @@ def minOperations(n):
         a method that calculates the fewest number of operations needed
         to result in exactly n H characters in the file
 
-    
+
         n: number of operations
 
         Return as an int the number of operations
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
 
