600e5a689ca5f9dacf2bc8ce633520c648cfe13c
marlyne <=>
2024-05-31 11:32:05 +0200

    minimumOperations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    """
+        a method that calculates the fewest number of operations needed
+        to result in exactly n H characters in the file
+
+    
+        n: number of operations
+
+        Return as an int the number of operations
+    """
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
