0c192096e1d89420166c31585f9683a581e07427
marlyne <=>
2024-06-07 11:41:34 +0200

    declaring variables outside the for loop

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -67,7 +67,7 @@ void topple(int grid[3][3])
         }
     }
 
-    for (i = 0; i < 3; i++)  // Nouvelle itération pour éviter la redéfinition de i
+    for (i = 0; i < 3; i++)  /** Nouvelle itération pour éviter la redéfinition de i */
     {
         for (j = 0; j < 3; j++)
         {
@@ -102,7 +102,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
         topple(new_grid);
     }
 
-    for (i = 0; i < 3; i++)  // Nouvelle itération pour éviter la redéfinition de i
+    for (i = 0; i < 3; i++)  /** Nouvelle itération pour éviter la redéfinition de i */
     {
         for (j = 0; j < 3; j++)
         {
