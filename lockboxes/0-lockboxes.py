1c9f155e5625dce03438aa11137f7a5b1f47000c
marlyne <=>
2025-01-03 20:37:09 +0100

    fix pycodestyle errors

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -12,16 +12,15 @@ def canUnlockAll(boxes):
     """
     n = len(boxes)
     opened_boxes = [False] * n
-    opened_boxes[0] = True      #boxes[0] is already unlocked
-    keys = boxes[0]      #the key of the first box
+    opened_boxes[0] = True      # boxes[0] is already unlocked
+    keys = boxes[0]      # the key of the first box
 
     # check available keys
     while keys:
-        current_key = keys.pop()    #retrieve a key from the stack
-        #check if the box associated to this key is opened or not
+        current_key = keys.pop()    # retrieve a key from the stack
+        # check if the box associated to this key is opened or not
         if current_key < n and not opened_boxes[current_key]:
-            opened_boxes[current_key] = True    #mark the box as opened
-            keys.extend(boxes[current_key])  #add all the keys to the stack
-            
+            opened_boxes[current_key] = True    # mark the box as opened
+            keys.extend(boxes[current_key])  # add all the keys to the stack
+
     return all(opened_boxes)
-    
