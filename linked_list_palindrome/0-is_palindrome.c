8c1341a4a7fdf46281facec4ca79e2d6df027c31
Marlyne Ihorimbere <113862900+Nelyrma@users.noreply.github.com>
2025-02-09 13:41:02 +0100

    linked list palindrome

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * validate_palindrome - Recursively checks if a singly linked list is a palindrome.
+ * @front: Double pointer to the head of the list.
+ * @back: Pointer to the current node in recursion (moving towards tail).
+ *
+ * Return: 1 if it's a palindrome, 0 otherwise.
+ */
+int validate_palindrome(listint_t **front, listint_t *back)
+{
+    int is_valid;
+
+    if (!back)
+        return (1);
+
+    is_valid = validate_palindrome(front, back->next);
+
+    if (!is_valid || (*front)->n != back->n)
+        return (0);
+
+    *front = (*front)->next;
+
+    return (1);
+}
+
+/**
+ * is_palindrome - Determines if a singly linked list is a palindrome.
+ * @head: Double pointer to the head of the list.
+ *
+ * Return: 1 if it's a palindrome, 0 otherwise.
+ */
+int is_palindrome(listint_t **head)
+{
+    if (!head || !*head)
+        return (1);
+
+    return (validate_palindrome(head, *head));
+}
