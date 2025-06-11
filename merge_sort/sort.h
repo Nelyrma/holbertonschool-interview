012c151b6fee07b83566a0919c767ddd693d122e
Nelyrma <marlyne.ihorimbere@gmail.com>
2025-06-11 21:58:17 +0200

    Write a function that sorts an array of integers in ascending order using the Merge Sort algorithm

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -1,9 +1,11 @@
-#ifndef MEEGE_SORT
-#define MEEGE_SORT
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
 
 void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void merge(int *array, int *copy, int left, int right, int mid);
 void split(int *array, int *copy, int left, int right);
 
-#endif
+#endif /* SORT_H */
